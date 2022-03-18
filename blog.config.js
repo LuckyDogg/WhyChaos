2cd63ac0dec9934e26a16d311e7404bfc55121fa
luckyDogg <wilson.wang.hc@gmail.com>
2022-03-18 14:52:24 +0800

    modify signature

--- blog.config.js
+++ blog.config.js
@@ -50,7 +50,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '5fd11ffd-08a1-423e-beac-7ada9b842e7b', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
