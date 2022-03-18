14652a1352086e5c29a4caaf1f6fbcddc58eaf31
luckyDogg <wilson.wang.hc@gmail.com>
2022-03-18 15:10:23 +0800

    remove black cube

--- components/Header.js
+++ components/Header.js
@@ -69,6 +69,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <div className="flex items-center">
           <Link href="/">
             <a aria-label={BLOG.title}>
+            ✨{BLOG.title}
               {/* <div className="h-6">
                 <svg
                   width="24"
